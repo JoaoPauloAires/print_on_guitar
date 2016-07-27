4de309e753bfecc4b249cc69264ca56839bf79e5
JoaoPauloAires <jpaulo.aires@gmail.com>
2016-07-27 14:55:24 -0300

    First try on extracting gp notes, it failed, but committing for further purposes.

--- turn_gp_to_data.py
+++ turn_gp_to_data.py
@@ -2,21 +2,33 @@ import sys
 import guitarpro as gp
 
 def parse_gp(file_path):
-	# Parse gp and extract a list of notes.
+    # Parse gp and extract a list of notes.
 
-	parsed_file = gp.parse(file_path)
+    parsed_file = gp.parse(file_path)
 
-	#ToDo: Extract notes.
-	
+    #ToDo: Extract notes.
+
+    # First test on extracting notes. (FAIL)
+
+    gp_reader = gp.GPFileBase(open(file_path, 'r'))
+
+    while 1:
+
+        try:
+            print gp_reader.read('s', 1)
+        except:
+            print "End of file."
+            break
+
 
 if __name__ == "__main__":
-	
-	if len(sys.argv) > 1:
+    
+    if len(sys.argv) > 1:
 
-		parse_gp(sys.argv[1])
-	
-	else:
+        parse_gp(sys.argv[1])
+    
+    else:
 
-		input_file = "data/iron_maiden-flight_of_icarus.gp4"
-		parse_gp(input_file)
-	
+        input_file = "data/iron_maiden-flight_of_icarus.gp4"
+        parse_gp(input_file)
+    
